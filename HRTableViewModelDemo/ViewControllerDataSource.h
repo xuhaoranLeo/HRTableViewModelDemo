bab530944d19aabcb908759adfaed39ce58772a3
许昊然 <liang_andy@163.com>
2016-06-25 16:13:36 +0800

    no message

--- HRTableViewModelDemo/ViewControllerDataSource.h
+++ HRTableViewModelDemo/ViewControllerDataSource.h
@@ -10,5 +10,13 @@
 #import "HRTableViewModel.h"
 
 @interface ViewControllerDataSource : NSObject
+/**
+ *  数据转换TableView模型数组
+ *
+ *  @param data     数据源
+ *  @param delegate VC
+ *
+ *  @return TableView可用的数组
+ */
 + (NSArray <HRTableViewModel *> *)reformDataToSectionModelArray:(id)data delegate:(id)delegate;
 @end
